66538e5a4ec45f9c2333d940ff670bd6d6eb6ea5
Anurag Pandey <anurag.pndy@gmail.com>
2023-05-20 15:33:18 +0530

    Completed

---
+++ config/mongoose.js
@@ -0,0 +1,15 @@
+//require the library
+const mongoose=require('mongoose');
+//connect to the database
+mongoose.connect('mongodb://localhost/habit_tracker_db', { useNewUrlParser: true , useUnifiedTopology: true });
+//aquire the connection to (check if it is successfull)
+const db=mongoose.connection;
+// on error
+db.on('error', console.error.bind(console, 'Error connecting to DataBase'));
+// up and running then print the message
+db.once('open', function()
+{
+    console.log('Successfully Connected to the database');
+})
+
+ module.exports = db;
